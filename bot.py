485cecbe01266d6c3fd3d714b83d1839c4ff8591
defaustok <118307833+defaustok@users.noreply.github.com>
2022-11-16 00:09:46 +0000

    apy_import

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
-import pandas
+import os
+from binance.client import Client
+from datetime import datetime, timedelta
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sn
+from binance_getprice import *
+from apyfromdefillama import *
+import mplfinance as mpf
+from decouple import config
+api_key = config('api_key')
+api_secret = config('api_secret')
+client = Client(api_key, api_secret)
+
+btc = get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE, '10 Nov, 2022', '15 Nov, 2022')
+apy = import_apy('WETH-USDC 0.3%','Arbitrum','v3')
+new = btc.join(apy, how = 'left').ffill()
+new = new[['Open', 'apy']]
+new
